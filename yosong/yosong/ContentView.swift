df17ce8094105b1edb549d59dac9cf92ba5da384
송영모 <mooyoung2309@gmail.com>
2022-04-22 13:47:20 +0900

    feat: 프로젝트 생성

---
+++ yosong/yosong/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  yosong
+//
+//  Created by 송영모 on 2022/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ yosong/yosong/yosongApp.swift
@@ -0,0 +1,17 @@
+//
+//  yosongApp.swift
+//  yosong
+//
+//  Created by 송영모 on 2022/04/22.
+//
+
+import SwiftUI
+
+@main
+struct yosongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
